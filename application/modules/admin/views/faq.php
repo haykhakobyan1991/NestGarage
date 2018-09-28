59d763dba257b6df667c1c614ccc2979ffe29d1e
hayk <haikhakobyan2@gmail.com>
2018-09-28 19:25:29 +0400

    hayk

--- application/modules/admin/views/faq.php
+++ application/modules/admin/views/faq.php
@@ -12,10 +12,7 @@
 		<div class="d-none alert alert-danger mt-md-3 mt-3" role="alert"></div>
 		<div class="d-none alert alert-success mt-md-3 mt-3" role="alert"></div>
 		<form>
-
-
 			<div class="row">
-
 				<div class="col-sm-6 pt-md-3 lang1_col">
 					<h5 class="title">Language 1</h5>
 					<?
@@ -23,19 +20,22 @@
 					foreach ($result as $value) :
 						if ($value['language_id'] == 1) {
 							?>
-							<div class="card mt-3 pt-md-3">
+							<div class="card mt-3 pt-md-3 first_card">
 								<div class="card-body">
+									<div style="text-align: right;" class="mbk-3 mb-md-3 delete_button lang1">
+										<button data-id="<?= $i ?>" type="button" class="delete btn btn-warning btn-sm">
+											-
+										</button>
+									</div>
 									<div class="form-group mt-3 pt-md-3">
 										<label>FAQ title</label>
 										<input value="<?= $value['title'] ?>" type="text" class="form-control"
-											   name="title_1[<?=$i?>]" placeholder="Enter title">
+											   name="title_1[<?= $i ?>]" placeholder="Enter title">
 
 										<label>FAQ text</label>
 										<textarea type="text" class="form-control" rows="6"
-												  name="text_1[<?=$i?>]"
+												  name="text_1[<?= $i ?>]"
 												  placeholder="Enter  Text"><?= $value['text'] ?></textarea>
-
-
 									</div>
 								</div>
 							</div>
@@ -47,62 +47,54 @@
 							class="add_l  mt-3 mt-md-3 btn btn-outline-success btn-lg btn-block">add new block
 					</button>
 				</div>
-
 				<div class="col-sm-6 pt-md-3 lang2_col"
 					 style="padding-bottom: 10px;background: #46404059;border-radius: 5px;">
 					<h5 class="title">Language 2</h5>
-
 					<?
 					$j = 1;
 					foreach ($result as $value) :
-					if ($value['language_id'] == 2) {
-					?>
-					<div class="card mt-3 pt-md-3">
-						<div class="card-body">
-							<div class="form-group mt-3 pt-md-3">
-								<label>FAQ title</label>
-								<input value="<?= $value['title'] ?>"  type="text" class="form-control"
-									   name="title_2[<?=$j?>]" placeholder="Enter title">
-
-								<label>FAQ text</label>
-								<textarea type="text" class="form-control" rows="6"
-										  name="text_2[<?=$j?>]" placeholder="Enter Text"><?= $value['text'] ?></textarea>
-
-
+						if ($value['language_id'] == 2) {
+							?>
+							<div class="card mt-3 pt-md-3 second_card">
+								<div class="card-body">
+									<div style="text-align: right;" class="mb-3 mb-md-3 delete_button lang2">
+										<button data-id="<?= $j ?>" type="button" class="delete btn btn-warning btn-sm">
+											-
+										</button>
+									</div>
+									<div class="form-group mt-3 pt-md-3">
+										<label>FAQ title</label>
+										<input value="<?= $value['title'] ?>" type="text" class="form-control"
+											   name="title_2[<?= $j ?>]" placeholder="Enter title">
+										<label>FAQ text</label>
+										<textarea type="text" class="form-control" rows="6"
+												  name="text_2[<?= $j ?>]"
+												  placeholder="Enter Text"><?= $value['text'] ?></textarea>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-
-						<?
-						$j++;
-					}
+							<?
+							$j++;
+						}
 					endforeach; ?>
-
-
 					<button style="position: absolute;bottom: -63px;width: 94%;" type="button"
 							class="add_2  mt-3 mt-md-3 btn btn-outline-success btn-lg btn-block">add new block
 					</button>
 				</div>
-
-
 			</div>
 		</form>
 	</div>
-
 	<input id="submit" class="btn btn-primary   float-right" type="submit" value="Submit"
 		   style="margin-top: -20px;margin-bottom: 10px;">
 </div>
-
-
 <script>
 	$(document).ready(function () {
 		var i = '<?=($i == '' ? 2 : $i)?>';
 		var j = 2;
-
 		$('.add_l').click(function () {
-			$('.lang1_col').append('<div class="card mt-3 pt-md-3">\n' +
+			$('.lang1_col').append('<div class="card mt-3 pt-md-3 second_div">\n' +
 				'<div class="card-body">\n' +
-				'<div style="text-align: right;" class="mb-3 mb-md-3 "><button type="button" class="delete btn btn-warning btn-sm">-</button></div>\n' +
+				'<div style="text-align: right;" class="mb-3 mb-md-3 delete_button"><button type="button" class="delete btn btn-warning btn-sm">-</button></div>\n' +
 				'<div class="form-group mt-3 pt-md-3">\n' +
 				'<label>FAQ title</label>\n' +
 				'<input value="" type="text" class="form-control"\n' +
@@ -114,21 +106,15 @@
 				'</div>\n' +
 				'</div>\n' +
 				'</div>');
-
 			$('html, body').animate({
 				scrollTop: '+=500px'
 			}, 800);
-
 			i++;
 		});
-
-
-
 		$('.add_2').click(function () {
-
-			$('.lang2_col').append('<div class="card mt-3 pt-md-3">\n' +
+			$('.lang2_col').append('<div class="card mt-3 pt-md-3 second_div">\n' +
 				'<div class="card-body">\n' +
-				'<div style="text-align: right;" class="mb-3 mb-md-3 "><button type="button" class="delete btn btn-warning btn-sm">-</button></div>\n' +
+				'<div style="text-align: right;" class="mb-3 mb-md-3 delete_button"><button type="button" class="delete btn btn-warning btn-sm">-</button></div>\n' +
 				'<div class="form-group mt-3 pt-md-3">\n' +
 				'<label>FAQ title</label>\n' +
 				'<input value="" type="text" class="form-control"\n' +
@@ -140,20 +126,22 @@
 				'</div>\n' +
 				'</div>\n' +
 				'</div>');
-
-
-
 			$('html, body').animate({
 				scrollTop: '+=500px'
 			}, 800);
-
 			j++;
 		});
-
-
 	});
-
 	$(document).on('click', '.delete', function () {
-		$(this).parent('div').parent('div').parent('.card').fadeOut();
+		$(this).parent('div').parent('div').parent('.card').remove();
 	})
+</script>
+<script>
+	$(document).ready(function () {
+		$('.delete').each(function () {
+			if ($(this).data('id') == 1) {
+				$(this).remove();
+			}
+		});
+	});
 </script>
